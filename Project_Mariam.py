12a6e8fcf5130efb968d81231fd729c359c76194
Mariam <77588576+mariam222-cypro@users.noreply.github.com>
2023-05-18 17:26:38 +0300

    Add files via upload

---
+++ Project_Mariam.py
@@ -0,0 +1,79 @@
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+import sys
+sys.path.append('/opt/airflow/includes')
+import queries
+from airflow.decorators import task
+from airflow.operators.python_operator import BranchPythonOperator
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime 
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators import *
+from airflow.providers import *
+from airflow.providers.amazon.aws.transfers.sql_to_s3 import *
+import emp_dim_insert_update 
+
+from airflow.operators.dummy import DummyOperator
+from airflow.decorators import task
+
+@task.branch(task_id="branch_task")
+def branch_func(ids_to_updatee):
+    if ids_to_updatee == '':
+        return ["dumm"]
+    else  :
+        return ["update_task_to_snowflake"]
+    
+    
+with DAG(
+    "Project_Mariam",
+    start_date=datetime(2023,5,14), 
+    catchup=False,
+    schedule='@yearly'
+    
+) as dag:
+
+    sql_to_s3_task_empsal = SqlToS3Operator(
+        task_id="sql_to_s3_task_empsal",
+        sql_conn_id='mariam_postgres_conn',
+        aws_conn_id='mariam_aws_conn',
+        query='SELECT * FROM finance.emp_sal',
+        s3_bucket='staging.emp.data',
+        s3_key='mariam_emp_sal.csv',
+        replace=True    
+    )
+    sql_to_s3_task_empdetails = SqlToS3Operator(
+        task_id="sql_to_s3_task_empdetails",
+        sql_conn_id='mariam_postgres_conn',
+        aws_conn_id='mariam_aws_conn',
+        query='SELECT * FROM hr.emp_details',
+        s3_bucket='staging.emp.data',
+        s3_key='mariam_emp_details.csv',
+        replace=True
+    )
+
+    join_and_detect_new_or_changed_rows = emp_dim_insert_update.join_and_detect_new_or_changed_rows()
+
+    branch_op = branch_func("{{ ti.xcom_pull(task_ids='join_and_detect_new_or_changed_rows', key='ids_to_update') }}")
+
+    update_task_to_snowflake = SnowflakeOperator(
+        task_id='update_task_to_snowflake',
+        sql=queries.UPDATE_DWH_EMP_DIM("{{ ti.xcom_pull(task_ids='join_and_detect_new_or_changed_rows', key='ids_to_update') }}"),
+        snowflake_conn_id='mariam_snowfl_conn'
+    )
+    insert_task_to_snowflake = SnowflakeOperator(
+        task_id="insert_task_to_snowflake",
+        sql=queries.INSERT_INTO_DWH_EMP_DIM("{{ ti.xcom_pull(task_ids='join_and_detect_new_or_changed_rows', key='rows_to_insert') }}") ,
+        snowflake_conn_id='mariam_snowfl_conn',
+        trigger_rule='none_failed'
+    )
+
+    dummy=DummyOperator(task_id="dumm")
+
+
+
+    [sql_to_s3_task_empsal,sql_to_s3_task_empdetails] >>join_and_detect_new_or_changed_rows>>branch_op>>[update_task_to_snowflake,dummy]>>insert_task_to_snowflake
+
+
+
+
---
+++ queries.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+#################################### AWS RDS Queries ###################################
+SELECT_EMP_SAL = """
+select * from finance.emp_sal
+"""
+
+SELECT_EMP_DETAIL = """
+select * from hr.emp_details
+"""
+
+################################## Snowflake DWH Queries ###############################
+SELECT_DWH_EMP_DIM = """
+select * from airflow_iti.dimensions.employee_dim
+"""
+
+def INSERT_INTO_DWH_EMP_DIM(rows_to_insert):
+    sql = f"""
+    INSERT INTO airflow_iti.dimensions.employee_dim (emp_id, name, marital_status, num_children, address, phone_number, job, hire_date, salary, effective_start_date, effective_end_date, active)
+    VALUES {rows_to_insert}"""
+    
+    return sql
+
+def UPDATE_DWH_EMP_DIM(ids_to_update):
+    sql = f"""
+    UPDATE airflow_iti.dimensions.employee_dim
+    SET effective_end_date = '{datetime.now().date().strftime("%Y-%m-%d")}',
+    active = FALSE
+    WHERE emp_id IN ({ids_to_update}) AND active = TRUE """
+    return sql
